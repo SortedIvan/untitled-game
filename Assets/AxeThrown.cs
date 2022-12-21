37833ab6a3d564d26e7780610ed13fd6aa5ef34a
Ovcharov,Ivan <450754@student.fontys.nl>
2022-12-21 23:40:24 +0100

    update: Added axe rotation based on direction

--- Assets/AxeThrown.cs
+++ Assets/AxeThrown.cs
@@ -8,13 +8,30 @@ public class AxeThrown : MonoBehaviour
     [SerializeField] private float _throwForce = 20f;
     [SerializeField] private float _throwSpeed = 30f;
 
+    private bool _hasToRotate;
+
     void Start()
     {
-        gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * (_throwForce * _throwSpeed));
+        //gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * (_throwForce * _throwSpeed));
+        _hasToRotate = true;
     }
 
     void Update()
     {
-        gameObject.GetComponent<Rigidbody>().AddTorque(Vector3.forward);
+        //gameObject.GetComponent<Rigidbody>().AddRelativeTorque(transform.right * _throwForce);
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (_hasToRotate)
+        {
+            gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3(Mathf.PI * 2, 0, 0);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        _hasToRotate = false;
     }
 }
